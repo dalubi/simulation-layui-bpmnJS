7fe4023f3b86d6f7529c1c4a0ffd5bb5c32c3a20
Zhengtianhao0601 <47098928+dalubi@users.noreply.github.com>
2021-01-14 11:56:21 +0800

    first commit

---
+++ src/main/java/com/ices/discrete_event_simulation/DiscreteEventSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.ices.discrete_event_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscreteEventSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscreteEventSimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ices/discrete_event_simulation/DiscreteEventSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ices.discrete_event_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscreteEventSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
